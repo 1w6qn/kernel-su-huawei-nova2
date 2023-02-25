2ad3a2dceea79765655dc6432dbbd4bbef3ddc26
1w6qn <2050903851@qq.com>
2023-02-25 14:59:19 +0800

    upd

---
+++ kernel/drivers/hisi/efuse/hisi_flash_hisee_otp.h
@@ -0,0 +1,24 @@
+/*
+ * hisi_flash_hisee_otp.h
+ *
+ * provides interfaces for writing hisee otp
+ *
+ * Copyright (c) 2001-2021, Huawei Tech. Co., Ltd. All rights reserved.
+ *
+ * chenli <chenli24@huawei.com>
+ *
+ */
+
+#ifndef	__HISI_FLASH_HISEE_OTP_H
+#define	__HISI_FLASH_HISEE_OTP_H
+
+#ifdef CONFIG_HISI_HISEE
+extern void creat_flash_otp_thread(void);
+extern void creat_flash_otp_init(void);
+#else
+static inline void creat_flash_otp_thread(void){}
+
+static inline void creat_flash_otp_init(void){}
+#endif
+
+#endif
